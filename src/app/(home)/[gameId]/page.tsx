4e3c6c69c36325f21e6233b3c14141c0a3f07986
TrippyRahul <rahulrawat.trippybug@gmail.com>
2025-03-26 15:10:38 +0530

    SLUG socket error

--- src/app/(home)/[gameId]/page.tsx
+++ src/app/(home)/[gameId]/page.tsx
@@ -1,9 +1,11 @@
+'use client';
+
 import GameFrame from "@/src/components/layout/GameFrame";
 import { getGameById } from "@/src/lib/actions";
-import React from "react";
+import { useSocket } from "@/src/lib/context/Socket";
+import { Events } from "@/src/lib/utils";
+import React, { useEffect, useState } from "react";
 
-export const dynamic = 'force-dynamic';
-export const fetchCache = 'force-no-store';
 
 interface PageProps {
   params: {
@@ -11,12 +13,30 @@ interface PageProps {
   };
 }
 
-const Page: React.FC<PageProps> = async ({ params }) => {
+const Page: React.FC<PageProps> = ({ params }) => {
   const { gameId } = params;
-  const src = await getGameById(gameId);
-  console.log("Game data PAGE", src);
+const socket = useSocket();
+const [gameData, setGameData] = useState<{ url?: string; message?: string } | null>(null);
+const [loading, setLoading] = useState(true);
 
-  return <GameFrame data={src} />;
+
+useEffect(() => {
+  if (!socket || !socket.connected  ||!gameId) return;
+
+  socket.emit(Events.PLAYGROUND_GAME_URL, { slug: gameId }, (res: any) => {
+    setLoading(false);
+    if (res.success) {
+      setGameData({ url: res.data.url });
+    } else {
+      setGameData({ message: res.message });
+    }
+  });
+}, [socket, gameId]);
+
+if (loading) return <p className="text-center">Loading game...</p>;
+if (!gameData) return <p className="text-center text-red-500">Something went wrong.</p>;
+
+return <GameFrame data={gameData} />;
 };
 
 export default Page;
